62b67a9235783c43b1ba0d7bd946c9280fb1f548
dwblair <donblair@gmail.com>
2015-07-29 23:36:51 +0000

    update

--- ping.py
+++ ping.py
@@ -43,6 +43,40 @@ def mymap():
     lat=4.601693
     lon=-74.065961
     
+    #sensorKeys=['G2qNwyjYGaCjRJzRQNOz','JxKlvngEL0HO75NldW2a','DJ6Ro7ala1hlV6jXq5bb']
+    sensorKeys=['JxKlvngEL0HO75NldW2a','DJ6Ro7ala1hlV6jXq5bb']
+    sensorLats=[4.621691, 4.601692]
+    sensorLons=[-74.064962,-74.075963]
+    
+    # print sensorKeys
+    
+    points=[]
+    
+    
+   # for key in sensorKeys:
+    for i in range(0,len(sensorKeys)):
+        key=sensorKeys[i]
+        lat=sensorLats[i]
+        lon=sensorLons[i]
+        phantURL='https://data.sparkfun.com/output/'+key+'.json?page=1'
+        #print phantURL
+        response=urllib2.urlopen(phantURL)
+        #print response
+        data=json.load(response)
+        last_reading=data[0]
+        temp=float(last_reading['temp'])
+        timeBits=last_reading['timestamp'].split('T')[1].split(':')
+        hour=timeBits[0]+':'+timeBits[1]
+         #print hour
+        date=last_reading['timestamp'].split('T')[0]
+    
+        batt=float(last_reading['batt'])
+        conduct=float(last_reading['conduct'])
+        point=[key,lat,lon,temp,batt,conduct, date, hour]
+        points.append(point)
+        
+    print points
+    
     phantKey='QGXKjpvEpWH268W1NvWD'
     phantURL='https://data.sparkfun.com/output/'+phantKey+'.json?page=1'
     
@@ -50,7 +84,7 @@ def mymap():
     data=json.load(response)
     last_reading=data[0]
     temp=float(last_reading['temp'])
-    print temp
+    #print temp
     
     timeBits=last_reading['timestamp'].split('T')[1].split(':')
     hour=timeBits[0]+':'+timeBits[1]
@@ -58,10 +92,10 @@ def mymap():
     date=last_reading['timestamp'].split('T')[0]
     #print date
     
-    point=lat,lon,temp,date,hour
+    myPoint=lat,lon,temp,date,hour
     mapCenter=mapLat,mapLon
     
-    return render_template('leaf.html',point=point,mapCenter=mapCenter)
+    return render_template('leaf.html',point=myPoint,mapCenter=mapCenter,points=points)
 
 @app.route("/crowd", methods=['GET', 'POST'])
 def crowd():
@@ -92,6 +126,14 @@ def hello():
 def crowdmap():
     return 'Hello map'
 #app.run(host=os.environ['IP'],port=int(os.environ['PORT']))
+
+@app.route("/ping/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
 
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT',8080)))
